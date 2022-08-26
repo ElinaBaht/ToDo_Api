44eec82fd4fe5266b8bb380267dac6e896619049
Elina <101188109+ElinaBaht@users.noreply.github.com>
2022-08-26 20:42:49 +0300

    Update Program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -5,10 +5,10 @@ using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
